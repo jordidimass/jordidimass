2d168881c969f804857cc7730d126a66018b9743
jordi <jordi.dimas@gmail.com>
2024-11-03 22:01:56 -0600

    fixed mobile about scroll

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,19 +28,21 @@ export default function AboutPage() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-orange-100 p-8">
-      <h1
-        className={`text-4xl lowercase font-bold mb-4 transition-all duration-1000 opacity-0 transform translate-y-[-20px]
-          ${isMounted ? "opacity-100 translate-y-0" : ""} 
-          ${isHovered ? "scale-110 text-red-600" : "text-gray-800"}`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        About Me
-      </h1>
-      <p className="text-2xl lowercase text-center max-w-2xl text-black">
-        I am a Software Developer from Guatemala, with a passion for physics, systems, computer interfaces and computer science. I never stop learning and constantly expand my knowledge, as I believe that connecting with inspiring individuals and challenging projects fuels my growth. I am eager to collaborate with other developers and contribute to the web development community.
-      </p>
+    <div className="fixed inset-0 flex bg-orange-100 items-center justify-center px-8">
+      <div className="w-full lg:w-1/2 lg:mx-auto lg:max-w-3xl text-center">
+        <h1
+          className={`text-4xl lowercase font-bold mb-4 transition-all duration-1000 opacity-0 transform translate-y-[-20px]
+            ${isMounted ? "opacity-100 translate-y-0" : ""} 
+            ${isHovered ? "scale-110 text-red-600" : "text-gray-800"}`}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          About Me
+        </h1>
+        <p className="text-2xl lowercase text-black">
+          I am a Software Developer from Guatemala, with a passion for physics, systems, computer interfaces and computer science. I never stop learning and constantly expand my knowledge, as I believe that connecting with inspiring individuals and challenging projects fuels my growth. I am eager to collaborate with other developers and contribute to the web development community.
+        </p>
+      </div>
     </div>
   );
 }
