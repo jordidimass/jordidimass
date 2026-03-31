080862b15c6ca7ec1e0a0c122def3a59f8d438a1
jordi <jordi.dimas@gmail.com>
2026-03-31 01:39:57 -0600

    galaxy a22

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <div className="fixed inset-0 w-screen h-screen flex flex-col lg:flex-row bg-brand-bg overflow-hidden">
       <Particles
         className="absolute inset-0 w-full h-full z-0"
-        quantity={450}
+        quantity={250}
         staticity={10}
         ease={60}
         color="#ffffff"
